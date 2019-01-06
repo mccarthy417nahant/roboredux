c6964628b210e5b9f1717d922cf95aa7e6781ec9
mccarthy417nahant <kevinmccarthy01@comcast.net>
2019-01-05 21:40:25 -0500

    replicate files from robofriends

---
+++ src/components/robots.js
@@ -0,0 +1,231 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Kevin McCarthy',
+    username: 'Kevin',
+    email: 'KevinM@boston-22.com'
+  },
+  {
+    id: 2,
+    name: 'Abby Bardin',
+    username: 'Abby',
+    email: 'Abby@boston-22.com'
+  },
+  {
+    id: 3,
+    name: 'Abigail Sims',
+    username: 'Abigail',
+    email: 'Abigail@boston-22.com'
+  },
+  {
+    id: 4,
+    name: 'Adam Curtis',
+    username: 'Adam',
+    email: 'Adam@boston-22.com'
+  },
+  {
+    id: 5,
+    name: 'Alan Dust',
+    username: 'Alan',
+    email: 'Alan@boston-22.com'
+  },
+  {
+    id: 6,
+    name: 'Amelia Kim',
+    username: 'Amelia',
+    email: 'Amelia@boston-22.com'
+  },
+  {
+    id: 7,
+    name: 'Brendon Shea',
+    username: 'Brendon',
+    email: 'Brendon@boston-22.com'
+  },
+  {
+    id: 8,
+    name: 'Brennan Callahan',
+    username: 'Brennan',
+    email: 'Brennan@boston-22.com'
+  },
+  {
+    id: 9,
+    name: 'Brian Burke',
+    username: 'Brian',
+    email: 'Brian@boston-22.com'
+  },
+  {
+    id: 10,
+    name: 'Brian Kim',
+    username: 'Brian Kim',
+    email: 'Brian.Kim@boston-22.com'
+  },
+  {
+    id: 11,
+    name: 'Corey Angelus',
+    username: 'Corey',
+    email: 'Corey@boston-22.com'
+  },
+  {
+    id: 12,
+    name: 'David Mui',
+    username: 'David',
+    email: 'David@boston-22.com'
+  },
+  {
+    id: 13,
+    name: 'Davis Ma',
+    username: 'Davis',
+    email: 'Davis@boston-22.com'
+  },
+  {
+    id: 14,
+    name: 'Donnie Waite',
+    username: 'Donnie',
+    email: 'Donnie@boston-22.com'
+  },
+  {
+    id: 15,
+    name: 'Ethan Schwelling',
+    username: 'Ethan',
+    email: 'Ethan@boston-22.com'
+  },
+  {
+    id: 16,
+    name: 'Harry Johnson',
+    username: 'Harry',
+    email: 'Harry@boston-22.com'
+  },
+  {
+    id: 17,
+    name: 'Jack Marston',
+    username: 'Jack',
+    email: 'Jack@boston-22.com'
+  },
+  {
+    id: 18,
+    name: 'Jay Ward',
+    username: 'Jay',
+    email: 'Jay@boston-22.com'
+  },
+  {
+    id: 19,
+    name: 'Joe Collins',
+    username: 'Joe',
+    email: 'Joe@boston-22.com'
+  },
+  {
+    id: 20,
+    name: 'John Trocchi',
+    username: 'John',
+    email: 'John@boston-22.com'
+  },
+  {
+    id: 21,
+    name: 'Josh Wyman',
+    username: 'Josh',
+    email: 'Josh@boston-22.com'
+  },
+  {
+    id: 22,
+    name: 'Junaid Siddiqui',
+    username: 'Junaid',
+    email: 'Junaid@boston-22.com'
+  },
+  {
+    id: 23,
+    name: 'Kentaro Kaneki',
+    username: 'Kentaro',
+    email: 'Kentaro@boston-22.com'
+  },
+  {
+    id: 24,
+    name: 'Lance Sun',
+    username: 'Lance',
+    email: 'Lance@boston-22.com'
+  },
+  {
+    id: 25,
+    name: 'Logan Jones',
+    username: 'Logan',
+    email: 'Logan@boston-22.com'
+  },
+  {
+    id: 26,
+    name: 'Marc Marino',
+    username: 'Marc',
+    email: 'Marc@boston-22.com'
+  },
+  {
+    id: 27,
+    name: 'Matthew Bowler',
+    username: 'Matt',
+    email: 'Matt@boston-22.com'
+  },
+  {
+    id: 28,
+    name: 'Michael Decroce-Movson',
+    username: 'Jake',
+    email: 'Jake@boston-22.com'
+  },
+  {
+    id: 29,
+    name: 'Nick Carty',
+    username: 'Nick',
+    email: 'Nick@boston-22.com'
+  },
+  {
+    id: 30,
+    name: 'Scott Sees',
+    username: 'Scott',
+    email: 'Scott@boston-22.com'
+  },
+  {
+    id: 31,
+    name: 'Theo Calianos',
+    username: 'Theo',
+    email: 'Theo@boston-22.com'
+  },
+  {
+    id: 32,
+    name: 'Nick Alberts',
+    username: 'NickA',
+    email: 'NickA@boston-22.com'
+  },
+  {
+    id: 33,
+    name: 'Celso',
+    username: 'Celso',
+    email: 'Celso@boston-22.com'
+  },
+  {
+    id: 34,
+    name: 'Casi Newell',
+    username: 'Casi',
+    email: 'Casi@boston-22.com'
+  },
+  {
+    id: 35,
+    name: 'Brianna Kincart',
+    username: 'Brianna',
+    email: 'Brianna@boston-22.com'
+  },
+  {
+    id: 36,
+    name: 'Dan Pickett',
+    username: 'Dan',
+    email: 'Dan@boston-22.com'
+  },
+  {
+    id: 37,
+    name: 'Amy Lynn',
+    username: 'Amy Lynn',
+    email: 'AmyLynn@boston-22.com'
+  },
+  {
+    id: 38,
+    name: 'Alex Schwartz',
+    username: 'Alex',
+    email: 'Alex@boston-22.com'
+  }
+
+];
