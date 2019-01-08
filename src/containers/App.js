dacd0aa35ed918754cbf3facedddf90aa17f8802
mccarthy417nahant <kevinmccarthy01@comcast.net>
2019-01-08 16:48:37 -0500

    combine reducers and add higher order functions with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,17 +5,21 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-import {setSearchField} from '../actions';
+import {setSearchField, requestRobots} from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
 const mapDispatchToProps = (dispatch) =>{
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
