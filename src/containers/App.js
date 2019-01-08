d8bc0905acb8073e79de0491f89f817ce4887a2e
mccarthy417nahant <kevinmccarthy01@comcast.net>
2019-01-08 17:01:04 -0500

    finish redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,25 +24,16 @@ const mapDispatchToProps = (dispatch) =>{
 }
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      robots: []
-    }
-  }
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+    this.props.onRequestRobots();
   }
 
   render() {
-      const { robots } = this.state;
-      const { searchField, onSearchChange } = this.props;
+      const { searchField, onSearchChange, robots, isPending } = this.props;
       const filteredRobots = robots.filter(robot =>{
         return robot.name.toLowerCase().includes(searchField.toLowerCase());
       })
-      return !robots.length ?
+      return isPending ?
         <h1>Loading</h1> :
         (
           <div className='tc'>
